157e11567842a867ece24a7c317a02cc5f4d062c
Alex <46456279+the-alex-b@users.noreply.github.com>
2023-06-22 11:20:17 +0200

    classifier accepts bytes now instead of image location

--- src/lentille/embedders/vision/Resnet50Embedder.py
+++ src/lentille/embedders/vision/Resnet50Embedder.py
@@ -9,15 +9,25 @@ class Resnet50Embedder(BaseEmbedder):
     """
     Always instantiate using: Resnet50Embedder.from_file(model_file.onnx)
     Models can be found here: https://github.com/onnx/models/tree/main/vision/classification/resnet
-
     """
 
     def _preprocess(self, image):
         return resnet50_preprocess.preprocess(image)
 
-    def classify(self, image_location: str) -> np.array:
-        image = Image.open(image_location)
-        preprocessed_image = self._preprocess(image)
+    def classify(
+        self, image_location: str = None, image_bytes: Image = None
+    ) -> np.array:
+        if image_location and image_bytes:
+            raise Exception(
+                "Please provide an image location or image bytes, not both."
+            )
+
+        if image_location:
+            image = Image.open(image_location)
+            preprocessed_image = self._preprocess(image)
+
+        if image_bytes:
+            preprocessed_image = self._preprocess(image_bytes)
 
         # Magic structure to be able to speak to onnx?
         input_name = self.session.get_inputs()[0].name
--- src/run.py
+++ src/run.py
@@ -1,6 +1,6 @@
 from lentille import Resnet50Embedder
 import numpy as np
-
+from PIL import Image
 
 embedder = Resnet50Embedder.from_file(
     "downloaded_models/resnet50/resnet50-v1-12-int8.onnx"
@@ -9,6 +9,8 @@ image_location = "test_data/cat.jpg"
 # img = Image.open("test_data/dog.jpg")
 # img = Image.open("test_data/segmented_airplane.png")
 
-result = embedder.classify(image_location)
+img = Image.open(image_location)
+
+result = embedder.classify(image_bytes=img)
 
 print(np.argmax(result))
